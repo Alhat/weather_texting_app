6644bb8d08cf777e94bdcc3d17d63d44457a4108
Sahil Alhat <sahilalhat@Sahils-MacBook-Pro.local>
2023-09-14 17:16:59 -0400

    installed brew + nvm + npm + twilio

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import weather_api
 def main():
     weatherData = weather_api.getWeatherData()
     #print('Current temperature in %s is %d℃' % (weatherData['location']['name'], weatherData['current']['temperature']))
-
     
+
 
 if __name__ == "__main__":
     main()
